c362a6b0d218dec0d7b222851f7f3871bba1a940
omar <omar@omar-VGN-NR350FE.(none)>
2014-02-11 23:30:42 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomina::Application.config.secret_key_base = 'e9f24b458ae455dfd470cb72693cfe812b5955893d25ffcdb3be222d2d6cb4ac9f3b117ed91ad84abe3846cba25668f35b5d4a169fd6ebeb350ca780ac0f51cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nomina::Application.config.session_store :cookie_store, key: '_nomina_session'
