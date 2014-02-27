fd4859a34c26f9d53c3d44eb98fd602009c3c34b
Angel Holberg Rosas Ayala <ahracnmr@gmail.com>
2014-02-27 14:02:42 -0600

    Agregue el modelo employee

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213225525) do
+ActiveRecord::Schema.define(version: 20140227200058) do
+
+  create_table "employees", force: true do |t|
+    t.string   "numero_de_empleado"
+    t.string   "nombre"
+    t.string   "apellido_paterno"
+    t.string   "apellido_materno"
+    t.string   "rfc"
+    t.string   "curp"
+    t.string   "e_mail"
+    t.string   "telefono"
+    t.string   "departamento"
+    t.integer  "clabe"
+    t.integer  "banco"
+    t.string   "numero_de_cuenta"
+    t.string   "puesto"
+    t.string   "riesgo_de_puesto"
+    t.string   "numero_seguridad_social"
+    t.string   "fecha_inicio_laboral"
+    t.string   "periocidad_pago"
+    t.string   "tipo_contrato"
+    t.decimal  "salario_diario_integrado"
+    t.decimal  "salario_base_cot_apor"
+    t.integer  "tipo_regimen"
+    t.string   "sexo"
+    t.string   "tipo_jornada"
+    t.integer  "antiguedad"
+    t.string   "calle"
+    t.string   "colonia"
+    t.string   "numero_exterior"
+    t.string   "numero_interior"
+    t.string   "municipio"
+    t.string   "estado"
+    t.string   "pais"
+    t.integer  "codigo_postal"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
